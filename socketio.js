adbe0f51cf79ea33d9b6e354722f976a9897f43d
青辰 <32487657+moxiefxj@users.noreply.github.com>
2020-03-06 22:00:12 +0800

    修改了表，将群表和用户表分离
    下一版着重实现正常的群聊和使用时间戳来获取新消息

--- socketio.js
+++ socketio.js
@@ -12,36 +12,40 @@ function getSocket(server){
        //接收登陆事件
        socket.on('login',async function(data){
             // 先判断是否已经有人在登录，如果有人在登录扽话，将其断开连接
-            let sqlStr1 = 'select * from table1 where isonline = ? and username =?'
+            let sqlStr1 = 'select * from users where isonline = ? and username =?'
             let result1 = await sqlQuery(sqlStr1,['true',data.username])
             if(result1.length > 0){
                 socket.to(result1[0].socketid).emit('logout',{content:'有人登录进来了，你被强制踢出去了！'})
             }
 
         //    修改数据库的登录信息(socketid,isonline)
-            let sqlStr = "update table1 set socketid=?,isonline=? where username =?" 
+            let sqlStr = "update users set socketid=?,isonline=? where username =?" 
             let result = await sqlQuery(sqlStr,[socket.id,'true',data.username])
             socket.emit('login',{
+                data:data,
                 state:'ok',
                 content:"登陆成功"
             })
-            
-            let sqlStr2 = "select * from table1"
-            //等待获取mysql查询结果
-            let result2 =await sqlQuery(sqlStr2)
-            io.sockets.emit('users',Array.from(result2))
+
+            // let sqlStr2 = "select * from users"
+            // //等待获取mysql查询结果
+            // let result2 =await sqlQuery(sqlStr2)
+            // io.sockets.emit('users',Array.from(result2))
 
             // 最新未接收的个人信息
+            // 下一版将改进为时间戳刷新消息
             let sqlStr3 = 'select * from chat where isread = ? and toid = ? '  
             let result3 =await sqlQuery(sqlStr3,['false',data.id])
             socket.emit('unreadMsg',Array.from(result3))
 
             // 加入群(房间)
-            // 获取所有的群
-            let sqlStr4 = 'select * from table1 where isgroup=?'
-            let result4 = await sqlQuery(sqlStr4,['true'])
+            let sqlStr4 = 'SELECT * FROM groupchat g WHERE id IN(SELECT groupid FROM users u,user_group ug WHERE u.id = ug.userid AND u.id = ?)'
+            let result4 = await sqlQuery(sqlStr4,data.id)
+            console.log(result4.id)
+            console.log(Array.from(result4))
+            socket.emit('room',Array.from(result4))
             Array.from(result4).forEach((item,index)=>{
-                socket.join(item.socketid)
+                socket.join(item.groupchat)  //加入房间
             })
 
        })
@@ -49,21 +53,21 @@ function getSocket(server){
     //    监听断开事件
        socket.on('disconnect',async function(){
             // 修改数据库的登录信息(socketid,isonline)
-            let sqlStr = "update table1 set socketid=?,isonline=? where socketid =?" 
+            let sqlStr = "update users set socketid=?,isonline=? where socketid =?" 
             let result = await sqlQuery(sqlStr,[null,null,socket.id])
        })
 
-       socket.on('users',async function(){
-            let sqlStr = "select * from table1"
-
+       socket.on('users',async function(data){
+            let sqlStr = "select * from users where id in (select friendid from friends where userid = ?)"
             //等待获取mysql查询结果
-            let result =await sqlQuery(sqlStr)
+            let result =await sqlQuery(sqlStr,[data.id])
             socket.emit('users',Array.from(result))
+
        })
 
        socket.on('sendMsg',async function(msg){
             // 判断收消息的人是否在线
-            let strSql = 'select * from table1 where username = ? and isonline = ?'
+            let strSql = 'select * from users where username = ? and isonline = ?'
             let result = await sqlQuery(strSql,[msg.toid.username,'true'])
             if(result.length > 0){
                 // 如果此人在线，那么直接发送消息
